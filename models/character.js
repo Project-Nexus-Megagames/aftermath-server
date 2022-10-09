fcd43adacd9e2048adf3b84b129a24b3251934e3
Franzi <34717874+franziskas1981@users.noreply.github.com>
2022-10-10 01:20:52 +0200

    WIP: models

--- models/character.js
+++ models/character.js
@@ -1,28 +1,27 @@
-const mongoose = require("mongoose"); // Mongo DB object modeling module
+const mongoose = require('mongoose'); // Mongo DB object modeling module
 
 // Global Constants
 const Schema = mongoose.Schema; // Destructure of Schema
 const ObjectId = mongoose.ObjectId; // Destructure of Object ID
-const nexusEvent = require("../middleware/events/events"); // Local event triggers
 
 const CharacterSchema = new Schema({
-  model: { type: String, default: "Character" },
-  playerName: { type: String, minlength: 1, maxlength: 50, required: true },
-  characterName: { type: String, minlength: 2, maxlength: 50, required: true },
-  username: { type: String, minlength: 2, maxlength: 50, required: true },
-  characterTitle: { type: String, maxlength: 50, default: "None" },
-  pronouns: { type: String },
-  bio: { type: String },
-  email: { type: String, required: true },
-  wiki: { type: String, default: "" },
-  timeZone: { type: String, default: "???" },
-  tags: [{ type: String }],
-  control: [{ type: String }],
-  color: { type: String, default: "ffffff" },
-  profilePicture: { type: String },
-  team: { type: Schema.Types.ObjectId, ref: "Team" },
+	model: { type: String, default: 'Character' },
+	playerName: { type: String, minlength: 1, maxlength: 50, required: true },
+	characterName: { type: String, minlength: 2, maxlength: 50, required: true },
+	username: { type: String, minlength: 2, maxlength: 50, required: true },
+	characterTitle: { type: String, maxlength: 50, default: 'None' },
+	pronouns: { type: String, default: '' },
+	bio: { type: String, default: 'None' },
+	email: { type: String, required: true, default: 'example@example.com' },
+	wiki: { type: String, default: '' },
+	timeZone: { type: String, default: '???' },
+	tags: [{ type: String, default: '' }],
+	control: [{ type: String, default: '' }],
+	color: { type: String, default: 'ffffff' },
+	profilePicture: { type: String, default: 'none' },
+	team: { type: ObjectId, ref: 'Team' }
 });
 
-const Character = mongoose.model("Character", CharacterSchema);
+const Character = mongoose.model('Character', CharacterSchema);
 
 module.exports = { Character };
--- models/team.js
+++ models/team.js
@@ -1,22 +1,22 @@
-const mongoose = require("mongoose"); // Mongo DB object modeling module
+const mongoose = require('mongoose'); // Mongo DB object modeling module
 
 // Global Constants
 const Schema = mongoose.Schema; // Destructure of Schema
-const ObjectId = mongoose.ObjectId; // Destructure of Object ID
 
 const unitSchema = new Schema({
-  submodel: { type: String, default: "Unit" },
-  name: { type: String, default: "Default" },
-  location: { type: String },
+	submodel: { type: String, default: 'Unit' },
+	name: { type: String, default: 'Default' },
+	location: { type: String, default: '' }
 });
 
 const TeamSchema = new Schema({
-  model: { type: String, default: "Team" },
-  units: [unitSchema],
-  description: { type: String, default: "Just a team of people, trying to survive after the apocalypse" },
-  conditions: [{ type: String }],
+	model: { type: String, default: 'Team' },
+	units: [unitSchema],
+	description: { type: String, default: 'Just a team of people, trying to survive after the apocalypse' },
+	conditions: [{ type: String, default: 'None' }],
+	location: { type: String, default: '' }
 });
 
-const Team = mongoose.model("Team", TeamSchema);
+const Team = mongoose.model('Team', TeamSchema);
 
 module.exports = { Team };
