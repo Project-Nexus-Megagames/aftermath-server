361f9ce7bbc8b7007adc4360fcc211cc8a2bdc79
Franzi <34717874+franziskas1981@users.noreply.github.com>
2022-11-14 00:55:34 +0100

    getting rid of some console logs

--- middleware/mongoDB/db.js
+++ middleware/mongoDB/db.js
@@ -6,13 +6,13 @@ const dbName = config.get('dbName');
 // const dbURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@candi.zx3mbs8.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 const dbURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.gev25ke.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
-console.log('OY', dbURI);
-
 const mongo_options = {
 	useUnifiedTopology: true,
 	useNewUrlParser: true
 };
 
 module.exports = function () {
-	mongoose.connect(dbURI, mongo_options).then(() => logger.info(`MongoDB Connected to ${dbName} database...`));
+	mongoose
+		.connect(dbURI, mongo_options)
+		.then(() => logger.info(`MongoDB Connected to ${dbName} database...`));
 };
